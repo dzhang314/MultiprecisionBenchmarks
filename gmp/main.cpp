24d71a4b907decd7eb8d1f3f7f239bb626026245
David K. Zhang <dzhang314@gmail.com>
2025-04-11 01:44:48 -0700

    simplify GMP test program

--- gmp/main.cpp
+++ gmp/main.cpp
@@ -21,20 +21,20 @@ static void axpy(mpf_t *y, mpf_t a, const mpf_t *x, std::size_t n,
     }
 }
 
-static void axpy_bench_1(benchmark::State &bs) {
+template <mp_bitcnt_t PRECISION>
+static void axpy_bench(benchmark::State &bs) {
 
     const std::size_t n = static_cast<std::size_t>(bs.range(0));
-    constexpr mp_bitcnt_t prec = 53;
 
     mpf_t *const y = static_cast<mpf_t *>(std::malloc(n * sizeof(mpf_t)));
-    for (std::size_t i = 0; i < n; ++i) { mpf_init2(y[i], prec); }
+    for (std::size_t i = 0; i < n; ++i) { mpf_init2(y[i], PRECISION); }
 
     mpf_t a;
-    mpf_init2(a, prec);
+    mpf_init2(a, PRECISION);
     mpf_set_d(a, 0.5);
 
     mpf_t *const x = static_cast<mpf_t *>(std::malloc(n * sizeof(mpf_t)));
-    for (std::size_t i = 0; i < n; ++i) { mpf_init2(x[i], prec); }
+    for (std::size_t i = 0; i < n; ++i) { mpf_init2(x[i], PRECISION); }
 
 #pragma omp parallel for schedule(static)
     for (std::size_t i = 0; i < n; ++i) {
@@ -47,7 +47,7 @@ static void axpy_bench_1(benchmark::State &bs) {
             mpf_set_d(y[i], 2.0 * static_cast<double>(i));
         }
         const auto start = std::chrono::high_resolution_clock::now();
-        axpy(y, a, x, n, prec);
+        axpy(y, a, x, n, PRECISION);
         const auto stop = std::chrono::high_resolution_clock::now();
         bs.SetIterationTime(
             std::chrono::duration<double>(stop - start).count());
@@ -69,149 +69,10 @@ static void axpy_bench_1(benchmark::State &bs) {
     std::free(y);
 }
 
-static void axpy_bench_2(benchmark::State &bs) {
-
-    const std::size_t n = static_cast<std::size_t>(bs.range(0));
-    constexpr mp_bitcnt_t prec = 103;
-
-    mpf_t *const y = static_cast<mpf_t *>(std::malloc(n * sizeof(mpf_t)));
-    for (std::size_t i = 0; i < n; ++i) { mpf_init2(y[i], prec); }
-
-    mpf_t a;
-    mpf_init2(a, prec);
-    mpf_set_d(a, 0.5);
-
-    mpf_t *const x = static_cast<mpf_t *>(std::malloc(n * sizeof(mpf_t)));
-    for (std::size_t i = 0; i < n; ++i) { mpf_init2(x[i], prec); }
-
-#pragma omp parallel for schedule(static)
-    for (std::size_t i = 0; i < n; ++i) {
-        mpf_set_d(x[i], static_cast<double>(i));
-    }
-
-    for (auto _ : bs) {
-#pragma omp parallel for schedule(static)
-        for (std::size_t i = 0; i < n; ++i) {
-            mpf_set_d(y[i], 2.0 * static_cast<double>(i));
-        }
-        const auto start = std::chrono::high_resolution_clock::now();
-        axpy(y, a, x, n, prec);
-        const auto stop = std::chrono::high_resolution_clock::now();
-        bs.SetIterationTime(
-            std::chrono::duration<double>(stop - start).count());
-#pragma omp parallel for schedule(static)
-        for (std::size_t i = 0; i < n; ++i) {
-            assert(mpf_cmp_d(y[i], 2.5 * static_cast<double>(i)) == 0);
-        }
-    }
-
-    bs.SetComplexityN(static_cast<benchmark::ComplexityN>(n));
-    bs.SetItemsProcessed(static_cast<std::int64_t>(n) * bs.iterations());
-
-    for (std::size_t i = 0; i < n; ++i) { mpf_clear(x[i]); }
-    std::free(x);
-
-    mpf_clear(a);
-
-    for (std::size_t i = 0; i < n; ++i) { mpf_clear(y[i]); }
-    std::free(y);
-}
-
-static void axpy_bench_3(benchmark::State &bs) {
-
-    const std::size_t n = static_cast<std::size_t>(bs.range(0));
-    constexpr mp_bitcnt_t prec = 156;
-
-    mpf_t *const y = static_cast<mpf_t *>(std::malloc(n * sizeof(mpf_t)));
-    for (std::size_t i = 0; i < n; ++i) { mpf_init2(y[i], prec); }
-
-    mpf_t a;
-    mpf_init2(a, prec);
-    mpf_set_d(a, 0.5);
-
-    mpf_t *const x = static_cast<mpf_t *>(std::malloc(n * sizeof(mpf_t)));
-    for (std::size_t i = 0; i < n; ++i) { mpf_init2(x[i], prec); }
-
-#pragma omp parallel for schedule(static)
-    for (std::size_t i = 0; i < n; ++i) {
-        mpf_set_d(x[i], static_cast<double>(i));
-    }
-
-    for (auto _ : bs) {
-#pragma omp parallel for schedule(static)
-        for (std::size_t i = 0; i < n; ++i) {
-            mpf_set_d(y[i], 2.0 * static_cast<double>(i));
-        }
-        const auto start = std::chrono::high_resolution_clock::now();
-        axpy(y, a, x, n, prec);
-        const auto stop = std::chrono::high_resolution_clock::now();
-        bs.SetIterationTime(
-            std::chrono::duration<double>(stop - start).count());
-#pragma omp parallel for schedule(static)
-        for (std::size_t i = 0; i < n; ++i) {
-            assert(mpf_cmp_d(y[i], 2.5 * static_cast<double>(i)) == 0);
-        }
-    }
-
-    bs.SetComplexityN(static_cast<benchmark::ComplexityN>(n));
-    bs.SetItemsProcessed(static_cast<std::int64_t>(n) * bs.iterations());
-
-    for (std::size_t i = 0; i < n; ++i) { mpf_clear(x[i]); }
-    std::free(x);
-
-    mpf_clear(a);
-
-    for (std::size_t i = 0; i < n; ++i) { mpf_clear(y[i]); }
-    std::free(y);
-}
-
-static void axpy_bench_4(benchmark::State &bs) {
-
-    const std::size_t n = static_cast<std::size_t>(bs.range(0));
-    constexpr mp_bitcnt_t prec = 209;
-
-    mpf_t *const y = static_cast<mpf_t *>(std::malloc(n * sizeof(mpf_t)));
-    for (std::size_t i = 0; i < n; ++i) { mpf_init2(y[i], prec); }
-
-    mpf_t a;
-    mpf_init2(a, prec);
-    mpf_set_d(a, 0.5);
-
-    mpf_t *const x = static_cast<mpf_t *>(std::malloc(n * sizeof(mpf_t)));
-    for (std::size_t i = 0; i < n; ++i) { mpf_init2(x[i], prec); }
-
-#pragma omp parallel for schedule(static)
-    for (std::size_t i = 0; i < n; ++i) {
-        mpf_set_d(x[i], static_cast<double>(i));
-    }
-
-    for (auto _ : bs) {
-#pragma omp parallel for schedule(static)
-        for (std::size_t i = 0; i < n; ++i) {
-            mpf_set_d(y[i], 2.0 * static_cast<double>(i));
-        }
-        const auto start = std::chrono::high_resolution_clock::now();
-        axpy(y, a, x, n, prec);
-        const auto stop = std::chrono::high_resolution_clock::now();
-        bs.SetIterationTime(
-            std::chrono::duration<double>(stop - start).count());
-#pragma omp parallel for schedule(static)
-        for (std::size_t i = 0; i < n; ++i) {
-            assert(mpf_cmp_d(y[i], 2.5 * static_cast<double>(i)) == 0);
-        }
-    }
-
-    bs.SetComplexityN(static_cast<benchmark::ComplexityN>(n));
-    bs.SetItemsProcessed(static_cast<std::int64_t>(n) * bs.iterations());
-
-    for (std::size_t i = 0; i < n; ++i) { mpf_clear(x[i]); }
-    std::free(x);
-
-    mpf_clear(a);
-
-    for (std::size_t i = 0; i < n; ++i) { mpf_clear(y[i]); }
-    std::free(y);
-}
+static void axpy_bench_1(benchmark::State &bs) { axpy_bench<53>(bs); }
+static void axpy_bench_2(benchmark::State &bs) { axpy_bench<103>(bs); }
+static void axpy_bench_3(benchmark::State &bs) { axpy_bench<156>(bs); }
+static void axpy_bench_4(benchmark::State &bs) { axpy_bench<208>(bs); }
 
 BENCHMARK(axpy_bench_1)
     ->UseManualTime()
