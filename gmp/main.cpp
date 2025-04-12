9277395af3efa8eb535f140c33af10abef29f11e
David K. Zhang <dzhang314@gmail.com>
2025-04-11 23:28:38 -0700

    add GEMV and GEMM to MPFR and GMP

--- gmp/main.cpp
+++ gmp/main.cpp
@@ -44,6 +44,42 @@ static void dot(mpf_t result, const mpf_t *x, const mpf_t *y, std::size_t n) {
 }
 
 template <mp_bitcnt_t PRECISION>
+static void gemv(mpf_t *y, const mpf_t *A, const mpf_t *x, std::size_t n) {
+#pragma omp parallel
+    {
+        mpf_t temp;
+        mpf_init2(temp, PRECISION);
+#pragma omp for schedule(static)
+        for (std::size_t i = 0; i < n; ++i) {
+            for (std::size_t j = 0; j < n; ++j) {
+                mpf_mul(temp, A[i * n + j], x[j]);
+                mpf_add(y[i], y[i], temp);
+            }
+        }
+        mpf_clear(temp);
+    }
+}
+
+template <mp_bitcnt_t PRECISION>
+static void gemm(mpf_t *C, const mpf_t *A, const mpf_t *B, std::size_t n) {
+#pragma omp parallel
+    {
+        mpf_t temp;
+        mpf_init2(temp, PRECISION);
+#pragma omp for schedule(static)
+        for (std::size_t i = 0; i < n; ++i) {
+            for (std::size_t k = 0; k < n; ++k) {
+                for (std::size_t j = 0; j < n; ++j) {
+                    mpf_mul(temp, A[i * n + k], B[k * n + j]);
+                    mpf_add(C[i * n + j], C[i * n + j], temp);
+                }
+            }
+        }
+        mpf_clear(temp);
+    }
+}
+
+template <mp_bitcnt_t PRECISION>
 static void axpy_bench(benchmark::State &bs) {
 
     const std::size_t n = static_cast<std::size_t>(bs.range(0));
@@ -133,6 +169,101 @@ static void dot_bench(benchmark::State &bs) {
     std::free(x);
 }
 
+template <mp_bitcnt_t PRECISION>
+static void gemv_bench(benchmark::State &bs) {
+
+    const std::size_t n = static_cast<std::size_t>(bs.range(0));
+
+    mpf_t *const y = static_cast<mpf_t *>(std::malloc(n * sizeof(mpf_t)));
+    for (std::size_t i = 0; i < n; ++i) { mpf_init2(y[i], PRECISION); }
+
+    mpf_t *const A = static_cast<mpf_t *>(std::malloc(n * n * sizeof(mpf_t)));
+    for (std::size_t i = 0; i < n * n; ++i) { mpf_init2(A[i], PRECISION); }
+
+#pragma omp parallel for schedule(static)
+    for (std::size_t i = 0; i < n * n; ++i) { mpf_set_d(A[i], 1.5); }
+
+    mpf_t *const x = static_cast<mpf_t *>(std::malloc(n * sizeof(mpf_t)));
+    for (std::size_t i = 0; i < n; ++i) { mpf_init2(x[i], PRECISION); }
+
+#pragma omp parallel for schedule(static)
+    for (std::size_t i = 0; i < n; ++i) { mpf_set_d(x[i], 2.5); }
+
+    for (auto _ : bs) {
+#pragma omp parallel for schedule(static)
+        for (std::size_t i = 0; i < n; ++i) { mpf_set_d(y[i], 0.0); }
+        const auto start = std::chrono::high_resolution_clock::now();
+        gemv<PRECISION>(y, A, x, n);
+        const auto stop = std::chrono::high_resolution_clock::now();
+        bs.SetIterationTime(
+            std::chrono::duration<double>(stop - start).count());
+#pragma omp parallel for schedule(static)
+        for (std::size_t i = 0; i < n; ++i) {
+            assert(mpf_cmp_d(y[i], 3.75 * static_cast<double>(n)) == 0);
+        }
+    }
+
+    bs.SetComplexityN(static_cast<benchmark::ComplexityN>(n));
+    bs.SetItemsProcessed(static_cast<std::int64_t>(n * n) * bs.iterations());
+
+    for (std::size_t i = 0; i < n; ++i) { mpf_clear(x[i]); }
+    std::free(x);
+
+    for (std::size_t i = 0; i < n * n; ++i) { mpf_clear(A[i]); }
+    std::free(A);
+
+    for (std::size_t i = 0; i < n; ++i) { mpf_clear(y[i]); }
+    std::free(y);
+}
+
+template <mp_bitcnt_t PRECISION>
+static void gemm_bench(benchmark::State &bs) {
+
+    const std::size_t n = static_cast<std::size_t>(bs.range(0));
+
+    mpf_t *const C = static_cast<mpf_t *>(std::malloc(n * n * sizeof(mpf_t)));
+    for (std::size_t i = 0; i < n * n; ++i) { mpf_init2(C[i], PRECISION); }
+
+    mpf_t *const A = static_cast<mpf_t *>(std::malloc(n * n * sizeof(mpf_t)));
+    for (std::size_t i = 0; i < n * n; ++i) { mpf_init2(A[i], PRECISION); }
+
+#pragma omp parallel for schedule(static)
+    for (std::size_t i = 0; i < n * n; ++i) { mpf_set_d(A[i], 1.5); }
+
+    mpf_t *const B = static_cast<mpf_t *>(std::malloc(n * n * sizeof(mpf_t)));
+    for (std::size_t i = 0; i < n * n; ++i) { mpf_init2(B[i], PRECISION); }
+
+#pragma omp parallel for schedule(static)
+    for (std::size_t i = 0; i < n * n; ++i) { mpf_set_d(B[i], 2.5); }
+
+    for (auto _ : bs) {
+#pragma omp parallel for schedule(static)
+        for (std::size_t i = 0; i < n * n; ++i) { mpf_set_d(C[i], 0.0); }
+        const auto start = std::chrono::high_resolution_clock::now();
+        gemm<PRECISION>(C, A, B, n);
+        const auto stop = std::chrono::high_resolution_clock::now();
+        bs.SetIterationTime(
+            std::chrono::duration<double>(stop - start).count());
+#pragma omp parallel for schedule(static)
+        for (std::size_t i = 0; i < n * n; ++i) {
+            assert(mpf_cmp_d(C[i], 3.75 * static_cast<double>(n)) == 0);
+        }
+    }
+
+    bs.SetComplexityN(static_cast<benchmark::ComplexityN>(n));
+    bs.SetItemsProcessed(static_cast<std::int64_t>(n * n * n) *
+                         bs.iterations());
+
+    for (std::size_t i = 0; i < n * n; ++i) { mpf_clear(B[i]); }
+    std::free(B);
+
+    for (std::size_t i = 0; i < n * n; ++i) { mpf_clear(A[i]); }
+    std::free(A);
+
+    for (std::size_t i = 0; i < n * n; ++i) { mpf_clear(C[i]); }
+    std::free(C);
+}
+
 static void axpy_bench_1(benchmark::State &bs) { axpy_bench<53>(bs); }
 static void axpy_bench_2(benchmark::State &bs) { axpy_bench<103>(bs); }
 static void axpy_bench_3(benchmark::State &bs) { axpy_bench<156>(bs); }
@@ -142,6 +273,16 @@ static void dot_bench_1(benchmark::State &bs) { dot_bench<53>(bs); }
 static void dot_bench_2(benchmark::State &bs) { dot_bench<103>(bs); }
 static void dot_bench_3(benchmark::State &bs) { dot_bench<156>(bs); }
 static void dot_bench_4(benchmark::State &bs) { dot_bench<208>(bs); }
+
+static void gemv_bench_1(benchmark::State &bs) { gemv_bench<53>(bs); }
+static void gemv_bench_2(benchmark::State &bs) { gemv_bench<103>(bs); }
+static void gemv_bench_3(benchmark::State &bs) { gemv_bench<156>(bs); }
+static void gemv_bench_4(benchmark::State &bs) { gemv_bench<208>(bs); }
+
+static void gemm_bench_1(benchmark::State &bs) { gemm_bench<53>(bs); }
+static void gemm_bench_2(benchmark::State &bs) { gemm_bench<103>(bs); }
+static void gemm_bench_3(benchmark::State &bs) { gemm_bench<156>(bs); }
+static void gemm_bench_4(benchmark::State &bs) { gemm_bench<208>(bs); }
 
 BENCHMARK(axpy_bench_1)
     ->UseManualTime()
@@ -205,4 +346,68 @@ BENCHMARK(dot_bench_4)
     ->Repetitions(3)
     ->RangeMultiplier(2)
     ->Range(1L << 8, 1L << 24)
+    ->DisplayAggregatesOnly();
+
+BENCHMARK(gemv_bench_1)
+    ->UseManualTime()
+    ->Complexity(benchmark::oNSquared)
+    ->Repetitions(3)
+    ->RangeMultiplier(2)
+    ->Range(1L << 4, 1L << 12)
+    ->DisplayAggregatesOnly();
+
+BENCHMARK(gemv_bench_2)
+    ->UseManualTime()
+    ->Complexity(benchmark::oNSquared)
+    ->Repetitions(3)
+    ->RangeMultiplier(2)
+    ->Range(1L << 4, 1L << 12)
+    ->DisplayAggregatesOnly();
+
+BENCHMARK(gemv_bench_3)
+    ->UseManualTime()
+    ->Complexity(benchmark::oNSquared)
+    ->Repetitions(3)
+    ->RangeMultiplier(2)
+    ->Range(1L << 4, 1L << 12)
+    ->DisplayAggregatesOnly();
+
+BENCHMARK(gemv_bench_4)
+    ->UseManualTime()
+    ->Complexity(benchmark::oNSquared)
+    ->Repetitions(3)
+    ->RangeMultiplier(2)
+    ->Range(1L << 4, 1L << 12)
+    ->DisplayAggregatesOnly();
+
+BENCHMARK(gemm_bench_1)
+    ->UseManualTime()
+    ->Complexity(benchmark::oNCubed)
+    ->Repetitions(3)
+    ->RangeMultiplier(2)
+    ->Range(1L << 3, 1L << 9)
+    ->DisplayAggregatesOnly();
+
+BENCHMARK(gemm_bench_2)
+    ->UseManualTime()
+    ->Complexity(benchmark::oNCubed)
+    ->Repetitions(3)
+    ->RangeMultiplier(2)
+    ->Range(1L << 3, 1L << 9)
+    ->DisplayAggregatesOnly();
+
+BENCHMARK(gemm_bench_3)
+    ->UseManualTime()
+    ->Complexity(benchmark::oNCubed)
+    ->Repetitions(3)
+    ->RangeMultiplier(2)
+    ->Range(1L << 3, 1L << 9)
+    ->DisplayAggregatesOnly();
+
+BENCHMARK(gemm_bench_4)
+    ->UseManualTime()
+    ->Complexity(benchmark::oNCubed)
+    ->Repetitions(3)
+    ->RangeMultiplier(2)
+    ->Range(1L << 3, 1L << 9)
     ->DisplayAggregatesOnly();
--- mpfr/main.cpp
+++ mpfr/main.cpp
@@ -45,6 +45,42 @@ static void dot(mpfr_t result, const mpfr_t *x, const mpfr_t *y,
 }
 
 template <mpfr_prec_t PRECISION>
+static void gemv(mpfr_t *y, const mpfr_t *A, const mpfr_t *x, std::size_t n) {
+#pragma omp parallel
+    {
+        mpfr_t temp;
+        mpfr_init2(temp, PRECISION);
+#pragma omp for schedule(static)
+        for (std::size_t i = 0; i < n; ++i) {
+            for (std::size_t j = 0; j < n; ++j) {
+                mpfr_mul(temp, A[i * n + j], x[j], MPFR_RNDF);
+                mpfr_add(y[i], y[i], temp, MPFR_RNDF);
+            }
+        }
+        mpfr_clear(temp);
+    }
+}
+
+template <mpfr_prec_t PRECISION>
+static void gemm(mpfr_t *C, const mpfr_t *A, const mpfr_t *B, std::size_t n) {
+#pragma omp parallel
+    {
+        mpfr_t temp;
+        mpfr_init2(temp, PRECISION);
+#pragma omp for schedule(static)
+        for (std::size_t i = 0; i < n; ++i) {
+            for (std::size_t k = 0; k < n; ++k) {
+                for (std::size_t j = 0; j < n; ++j) {
+                    mpfr_mul(temp, A[i * n + k], B[k * n + j], MPFR_RNDF);
+                    mpfr_add(C[i * n + j], C[i * n + j], temp, MPFR_RNDF);
+                }
+            }
+        }
+        mpfr_clear(temp);
+    }
+}
+
+template <mpfr_prec_t PRECISION>
 static void axpy_bench(benchmark::State &bs) {
 
     const std::size_t n = static_cast<std::size_t>(bs.range(0));
@@ -134,6 +170,111 @@ static void dot_bench(benchmark::State &bs) {
     std::free(x);
 }
 
+template <mpfr_prec_t PRECISION>
+static void gemv_bench(benchmark::State &bs) {
+
+    const std::size_t n = static_cast<std::size_t>(bs.range(0));
+
+    mpfr_t *const y = static_cast<mpfr_t *>(std::malloc(n * sizeof(mpfr_t)));
+    for (std::size_t i = 0; i < n; ++i) { mpfr_init2(y[i], PRECISION); }
+
+    mpfr_t *const A =
+        static_cast<mpfr_t *>(std::malloc(n * n * sizeof(mpfr_t)));
+    for (std::size_t i = 0; i < n * n; ++i) { mpfr_init2(A[i], PRECISION); }
+
+#pragma omp parallel for schedule(static)
+    for (std::size_t i = 0; i < n * n; ++i) {
+        mpfr_set_d(A[i], 1.5, MPFR_RNDF);
+    }
+
+    mpfr_t *const x = static_cast<mpfr_t *>(std::malloc(n * sizeof(mpfr_t)));
+    for (std::size_t i = 0; i < n; ++i) { mpfr_init2(x[i], PRECISION); }
+
+#pragma omp parallel for schedule(static)
+    for (std::size_t i = 0; i < n; ++i) { mpfr_set_d(x[i], 2.5, MPFR_RNDF); }
+
+    for (auto _ : bs) {
+#pragma omp parallel for schedule(static)
+        for (std::size_t i = 0; i < n; ++i) { mpfr_set_zero(y[i], +1); }
+        const auto start = std::chrono::high_resolution_clock::now();
+        gemv<PRECISION>(y, A, x, n);
+        const auto stop = std::chrono::high_resolution_clock::now();
+        bs.SetIterationTime(
+            std::chrono::duration<double>(stop - start).count());
+#pragma omp parallel for schedule(static)
+        for (std::size_t i = 0; i < n; ++i) {
+            assert(mpfr_cmp_d(y[i], 3.75 * static_cast<double>(n)) == 0);
+        }
+    }
+
+    bs.SetComplexityN(static_cast<benchmark::ComplexityN>(n));
+    bs.SetItemsProcessed(static_cast<std::int64_t>(n * n) * bs.iterations());
+
+    for (std::size_t i = 0; i < n; ++i) { mpfr_clear(x[i]); }
+    std::free(x);
+
+    for (std::size_t i = 0; i < n * n; ++i) { mpfr_clear(A[i]); }
+    std::free(A);
+
+    for (std::size_t i = 0; i < n; ++i) { mpfr_clear(y[i]); }
+    std::free(y);
+}
+
+template <mpfr_prec_t PRECISION>
+static void gemm_bench(benchmark::State &bs) {
+
+    const std::size_t n = static_cast<std::size_t>(bs.range(0));
+
+    mpfr_t *const C =
+        static_cast<mpfr_t *>(std::malloc(n * n * sizeof(mpfr_t)));
+    for (std::size_t i = 0; i < n * n; ++i) { mpfr_init2(C[i], PRECISION); }
+
+    mpfr_t *const A =
+        static_cast<mpfr_t *>(std::malloc(n * n * sizeof(mpfr_t)));
+    for (std::size_t i = 0; i < n * n; ++i) { mpfr_init2(A[i], PRECISION); }
+
+#pragma omp parallel for schedule(static)
+    for (std::size_t i = 0; i < n * n; ++i) {
+        mpfr_set_d(A[i], 1.5, MPFR_RNDF);
+    }
+
+    mpfr_t *const B =
+        static_cast<mpfr_t *>(std::malloc(n * n * sizeof(mpfr_t)));
+    for (std::size_t i = 0; i < n * n; ++i) { mpfr_init2(B[i], PRECISION); }
+
+#pragma omp parallel for schedule(static)
+    for (std::size_t i = 0; i < n * n; ++i) {
+        mpfr_set_d(B[i], 2.5, MPFR_RNDF);
+    }
+
+    for (auto _ : bs) {
+#pragma omp parallel for schedule(static)
+        for (std::size_t i = 0; i < n * n; ++i) { mpfr_set_zero(C[i], +1); }
+        const auto start = std::chrono::high_resolution_clock::now();
+        gemm<PRECISION>(C, A, B, n);
+        const auto stop = std::chrono::high_resolution_clock::now();
+        bs.SetIterationTime(
+            std::chrono::duration<double>(stop - start).count());
+#pragma omp parallel for schedule(static)
+        for (std::size_t i = 0; i < n * n; ++i) {
+            assert(mpfr_cmp_d(C[i], 3.75 * static_cast<double>(n)) == 0);
+        }
+    }
+
+    bs.SetComplexityN(static_cast<benchmark::ComplexityN>(n));
+    bs.SetItemsProcessed(static_cast<std::int64_t>(n * n * n) *
+                         bs.iterations());
+
+    for (std::size_t i = 0; i < n * n; ++i) { mpfr_clear(B[i]); }
+    std::free(B);
+
+    for (std::size_t i = 0; i < n * n; ++i) { mpfr_clear(A[i]); }
+    std::free(A);
+
+    for (std::size_t i = 0; i < n * n; ++i) { mpfr_clear(C[i]); }
+    std::free(C);
+}
+
 static void axpy_bench_1(benchmark::State &bs) { axpy_bench<53>(bs); }
 static void axpy_bench_2(benchmark::State &bs) { axpy_bench<103>(bs); }
 static void axpy_bench_3(benchmark::State &bs) { axpy_bench<156>(bs); }
@@ -143,6 +284,16 @@ static void dot_bench_1(benchmark::State &bs) { dot_bench<53>(bs); }
 static void dot_bench_2(benchmark::State &bs) { dot_bench<103>(bs); }
 static void dot_bench_3(benchmark::State &bs) { dot_bench<156>(bs); }
 static void dot_bench_4(benchmark::State &bs) { dot_bench<208>(bs); }
+
+static void gemv_bench_1(benchmark::State &bs) { gemv_bench<53>(bs); }
+static void gemv_bench_2(benchmark::State &bs) { gemv_bench<103>(bs); }
+static void gemv_bench_3(benchmark::State &bs) { gemv_bench<156>(bs); }
+static void gemv_bench_4(benchmark::State &bs) { gemv_bench<208>(bs); }
+
+static void gemm_bench_1(benchmark::State &bs) { gemm_bench<53>(bs); }
+static void gemm_bench_2(benchmark::State &bs) { gemm_bench<103>(bs); }
+static void gemm_bench_3(benchmark::State &bs) { gemm_bench<156>(bs); }
+static void gemm_bench_4(benchmark::State &bs) { gemm_bench<208>(bs); }
 
 BENCHMARK(axpy_bench_1)
     ->UseManualTime()
@@ -206,4 +357,68 @@ BENCHMARK(dot_bench_4)
     ->Repetitions(3)
     ->RangeMultiplier(2)
     ->Range(1L << 8, 1L << 24)
+    ->DisplayAggregatesOnly();
+
+BENCHMARK(gemv_bench_1)
+    ->UseManualTime()
+    ->Complexity(benchmark::oNSquared)
+    ->Repetitions(3)
+    ->RangeMultiplier(2)
+    ->Range(1L << 4, 1L << 12)
+    ->DisplayAggregatesOnly();
+
+BENCHMARK(gemv_bench_2)
+    ->UseManualTime()
+    ->Complexity(benchmark::oNSquared)
+    ->Repetitions(3)
+    ->RangeMultiplier(2)
+    ->Range(1L << 4, 1L << 12)
+    ->DisplayAggregatesOnly();
+
+BENCHMARK(gemv_bench_3)
+    ->UseManualTime()
+    ->Complexity(benchmark::oNSquared)
+    ->Repetitions(3)
+    ->RangeMultiplier(2)
+    ->Range(1L << 4, 1L << 12)
+    ->DisplayAggregatesOnly();
+
+BENCHMARK(gemv_bench_4)
+    ->UseManualTime()
+    ->Complexity(benchmark::oNSquared)
+    ->Repetitions(3)
+    ->RangeMultiplier(2)
+    ->Range(1L << 4, 1L << 12)
+    ->DisplayAggregatesOnly();
+
+BENCHMARK(gemm_bench_1)
+    ->UseManualTime()
+    ->Complexity(benchmark::oNCubed)
+    ->Repetitions(3)
+    ->RangeMultiplier(2)
+    ->Range(1L << 3, 1L << 9)
+    ->DisplayAggregatesOnly();
+
+BENCHMARK(gemm_bench_2)
+    ->UseManualTime()
+    ->Complexity(benchmark::oNCubed)
+    ->Repetitions(3)
+    ->RangeMultiplier(2)
+    ->Range(1L << 3, 1L << 9)
+    ->DisplayAggregatesOnly();
+
+BENCHMARK(gemm_bench_3)
+    ->UseManualTime()
+    ->Complexity(benchmark::oNCubed)
+    ->Repetitions(3)
+    ->RangeMultiplier(2)
+    ->Range(1L << 3, 1L << 9)
+    ->DisplayAggregatesOnly();
+
+BENCHMARK(gemm_bench_4)
+    ->UseManualTime()
+    ->Complexity(benchmark::oNCubed)
+    ->Repetitions(3)
+    ->RangeMultiplier(2)
+    ->Range(1L << 3, 1L << 9)
     ->DisplayAggregatesOnly();
